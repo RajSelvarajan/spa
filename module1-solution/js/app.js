d598fd72563162951e9a1de932e6ea23c6bc780a
Inbarajan Selvarajan <i_selvarajan@hotmail.com>
2016-10-14 17:49:09 -0400

    Final

---
+++ module1-solution/js/app.js
@@ -0,0 +1,47 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.lunchMenu = "";
+  $scope.lunchMessage = "";
+  $scope.lunchMessageStyle = ""; //OPTIONAL WORK
+  $scope.lunchMenuStyle = "";//OPTIONAL WORK
+
+  $scope.displayMessage = function () {
+    var messageString = "";
+    var separator =/\s*,\s*/;
+    var lunchMenuCount = calculateLunchMenuCount($scope.lunchMenu,separator);
+    if (lunchMenuCount==0) {
+      $scope.lunchMessage ="Please enter data first.";
+      $scope.lunchMessageStyle = "color: red;";
+      $scope.lunchMenuStyle = "border:1px solid red;";
+    }
+    else if (lunchMenuCount>3) {
+      $scope.lunchMessage ="Too much!";
+      $scope.lunchMessageStyle = "color: green;";
+      $scope.lunchMenuStyle = "border:1px solid green;";
+    }
+    else {
+      $scope.lunchMessage ="Enjoy!";
+      $scope.lunchMessageStyle = "color: green;";
+      $scope.lunchMenuStyle = "border:1px solid green;";
+    }
+  };
+  //  *** Please note that empty items are not considered towards the count ***
+  function calculateLunchMenuCount(string, separator) {
+    var lunchMenuArray = string.split(separator);
+    var count = 0;
+    for (var i = 0; i < lunchMenuArray.length; i++) {
+      if (lunchMenuArray[i].trim().length>0) {
+        count++
+      }
+    }
+    return count;
+  }
+}
+
+})();
